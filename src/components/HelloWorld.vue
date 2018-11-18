6e185cfe9c8406bb9a1dfc976be33a6947ff1193
hrkd <hiroki@hrkd.net>
2018-11-18 20:06:43 +0900

    wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,69 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Works</h2>
+    <h3>2018</h3>
+      <ul>
+        <li><a href="https://valu.is" target="_blank">VALU</a> | web front end development<span>from 2017</span></li>
+        <li><a href="https://matchbox.work" target="_blank">MATCH BOX</a> | web front end development<span>from 2017</span></li>
+        <li><a href="http://stillbyhand.jp/" target="_blank">STILL BY HAND</a> | web front end / WordPress development</li>
+        <li>ChatBot (Google dialog flow) client</li>
+      </ul>
+    <h3>2017</h3>
+      <ul>
+        <li>T-Shirt printing web service</li>
+        <li>Css frame work for ec-service</li>
+      </ul>
+    <h3>2016</h3>
+      <ul>
+        <li>WordPress development for ec-service</li>
+      </ul>
+    <h2>About me</h2>
+    <p>
+      児玉広樹<br>
+      北海道出身、東京在住のウェブフロントエンドエンジニア。Javascriptをひたすら書く人。Reactjsでwebサービス作ったりReactNativeで趣味アプリ作ったり。<br>
+      レガシーなフロントエンドの書き換えみたいな地味なのが結構好き。<br>
+      近頃はUIデザイン業に興味あり<br>
+      <br>
+      Hiroki Kodama<br>
+      Born in Hokkaido, live in Tokyo as a web front-end engineer. Javascript lover. Making web services with React.js and hobby app with React Native etc.<br>
+      <br>
+      主な経歴<br>
+      <ul>
+        <li>2018 <a href="https://prty.jp/" target="_blank">株式会社PARTY</a>と業務委託契約</li>
+        <li>2017 <a href="https://valu.is/company" target="_blank">株式会社VALU</a> と業務委託契約</li>
+        <li>2013 東京に移住・フリーランスとして活動を開始</li>
+        <li>2006 札幌市の広告制作会社に入社</li>
+      </ul>
+    </p>
+    <h2>Activity</h2>
+      <ul>
+        <li><a href="https://github.com/hrkd" target="_blank">GitHub</a></li>
+        <li><a href="https://twitter.com/_hrkd/" target="_blank">Twitter</a></li>
+      </ul>
   </div>
 </template>
 
@@ -14,5 +52,5 @@
     components: {
       HelloWorld
     }
-  };
+  }
 </script>
