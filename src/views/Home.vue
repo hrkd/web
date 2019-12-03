9e759a1b17bf8808549e6e341fe281a362b0b655
hrkd <hiroki@hrkd.net>
2019-12-03 14:52:13 +0900

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h2>Works</h2>
+    <h3>2019</h3>
+      <ul>
+        <li><a href="https://prty.jp/work/fuji-rock-19-by-softbank-5g" target="_blank">FUJI ROCK Festival 2019 App</a> | iOS development</li>
+      </ul>
     <h3>2018</h3>
       <ul>
         <li><a href="https://valu.is" target="_blank">VALU</a> | web front end development<span>from 2017</span></li>
@@ -19,16 +23,16 @@
       </ul>
     <h2>About me</h2>
     <p>
-      児玉広樹<br>
-      北海道出身、東京在住のウェブフロントエンドエンジニア。Javascriptをひたすら書く人。Reactjsでwebサービス作ったりReactNativeで趣味アプリ作ったり。<br>
-      レガシーなフロントエンドの書き換えみたいな地味なのが結構好き。<br>
-      近頃はUIデザイン業に興味あり<br>
+      児玉広樹 Hiroki Kodama<br>
+      北海道出身、東京在住のiOS/ウェブフロントエンドエンジニア。<br>
+      Swift / iOS / TypeScript / React / Python / Django<br>
       <br>
       Hiroki Kodama<br>
       Born in Hokkaido, live in Tokyo as a web front-end engineer. Javascript lover. Making web services with React.js and hobby app with React Native etc.<br>
       <br>
       主な経歴<br>
       <ul>
+        <li>2019 <a href="https://prty.jp/" target="_blank">株式会社PARTY</a>に入社</li>
         <li>2018 <a href="https://prty.jp/" target="_blank">株式会社PARTY</a>と業務委託契約</li>
         <li>2017 <a href="https://valu.is/company" target="_blank">株式会社VALU</a> と業務委託契約</li>
         <li>2013 東京に移住・フリーランスとして活動を開始</li>
